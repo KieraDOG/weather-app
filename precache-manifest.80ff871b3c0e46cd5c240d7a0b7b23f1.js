a35e77954e37eab63c9e5ccc85c2150fdeba5109
Long Zhao <zlong@outlook.com>
2020-06-10 18:29:35 +1000

    Updates

---
+++ precache-manifest.80ff871b3c0e46cd5c240d7a0b7b23f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637c2665853c6412421edf20c0cd7a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "158f2006144a314acffd",
+    "url": "/static/css/main.33518e1b.chunk.css"
+  },
+  {
+    "revision": "c2e0bc7f25b442673a02",
+    "url": "/static/js/2.6c91ff27.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6c91ff27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "158f2006144a314acffd",
+    "url": "/static/js/main.3520bcdd.chunk.js"
+  },
+  {
+    "revision": "5cfe63f3d0aa65dc782b",
+    "url": "/static/js/runtime-main.dbafcd4d.js"
+  }
+]);
